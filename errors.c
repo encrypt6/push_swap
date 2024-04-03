cdbffac9f99fe0bf851e780a0c0a52c6170d7af7
Eliza Sikira <elsikira@paul-f4Br9s1.clusters.42paris.fr>
2024-04-04 01:05:39 +0200

    INT_MIN / INT_MAX fixed

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: elsikira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:56:45 by elsikira          #+#    #+#             */
-/*   Updated: 2024/03/29 17:55:31 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/04/04 01:04:12 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ int	ft_not_int_error(int argc, char *argv[])
 	{
 		while (argv[i][j])
 		{
-			if (!ft_isdigit(argv[i][j]) && (argv[i][j] != '-' || argv[i][j] != '+'))
+			if (j == 0 && (argv[i][j] == '-' || argv[i][j] == '+'))
+				j++;
+			if (!ft_isdigit(argv[i][j]))
 				return (1);
 			j++;
 		}
@@ -33,23 +35,23 @@ int	ft_not_int_error(int argc, char *argv[])
 	return (0);
 }
 
-int	ft_bigger_than_int_error(int argc, char *argv[])
+int	ft_overunderflow_error(int argc, char *argv[])
 {
 	int	i;
+	long 	x;
 
 	i = 1;
 	while (i < argc)
 	{
-		long long int	num;
-
-		num = atoll(argv[i]);
-		if (num > INT_MAX || num < INT_MIN)
+		x = ft_atol(argv[i]);
+		if (x > INT_MAX)
+			return (1);
+		if (x < INT_MIN)
 			return (1);
 		i++;
 	}
-	return (0);
+      return (0);
 }
-
 /*int	ft_dupplicates_error(int argc, char *argv[])
 {
 	
@@ -63,7 +65,7 @@ int	ft_check_all_errors(int argc, char *argv[])
 {
 	if (ft_not_int_error(argc, argv))
 		return (1);
-	if (ft_bigger_than_int_error(argc, argv))
+	if (ft_overunderflow_error(argc, argv))
 		return (1);
 	/*if (ft_dupplicates_error(argc, argv))
 		return (1);*/

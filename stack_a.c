847a41bb9eeb1454008392d64c1ee0dbb963859b
Eliza Sikira <elsikira@bess-f2r1s13.clusters.42paris.fr>
2024-04-05 20:02:29 +0200

    start of stack a

--- stack_a.c
+++ stack_a.c
@@ -6,8 +6,18 @@
 /*   By: elsikira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 23:37:31 by elsikira          #+#    #+#             */
-/*   Updated: 2024/04/04 23:52:34 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/04/05 20:02:17 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+struct s_Node{
+	int			data;
+	struct Node	*next;
+};
+
+struct s_Node	*head = NULL;
+
+void	add_data_to_stack(int data) {
+	struct s_Node	*newNode = (struct Node)malloc(sizeof(struct Node));
+	if (newNode == NULL){
 

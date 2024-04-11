8b6b97dd16707ff33ddd344a264c0c58d1a4dea9
Eliza Sikira <elsikira@made-f0Dr11s16.clusters.42paris.fr>
2024-04-11 19:05:13 +0200

    norm almost done

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: elsikira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:19:09 by elsikira          #+#    #+#             */
-/*   Updated: 2024/04/11 18:29:02 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/04/11 19:04:50 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,65 +19,74 @@ struct	s_Node {
 
 struct	s_Node	*create_node(int data)
 {
-	struct	s_Node	*new_node = (struct s_Node*)malloc(sizeof(struct s_Node)); 
+	struct s_Node	*new_node;
+
+	new_node = (struct s_Node *)malloc(sizeof(struct s_Node));
 	new_node->data = data;
 	new_node->next = NULL;
-	return new_node;
+	return (new_node);
 }
 
-int is_empty(struct Node* top)
-{ 
-	return top == NULL;
-}
-
-void	push(struct Node** top, int data)
+int	is_empty(struct Node *top)
 {
-    struct Node* newNode = createNode(data); 
-    newNode->next = *top; 
-    *top = newNode; 
-    printf("%d pushed to the stack.\n", data); 
+	return (top == NULL);
 }
 
-int	pop(struct Node** top)
+void	push(struct Node **top, int data)
 {
-    if (isEmpty(*top)) 
+	struct Node	*new_node;
+	new node = create_node(data);
+	new_node->next = *top;
+	*top = new_node;
+	printf("%d pushed to the stack.\n", data);
+}
+
+int	pop(struct s_Node **top)
+{
+	struct s_Node	*temp;
+	int				popped;
+
+	if (is_empty(*top))
 	{
 		printf("Stack underflow!\n");
-		return INT_MIN; 
-    }
-	struct Node* temp = *top;
-	int popped = temp->data;
+		return (INT_MIN);
+	}
+	temp = top;
+	popped = temp->data;
 	*top = (*top)->next;
 	free(temp);
-	return popped;
+	return (popped);
 }
 
-// Function to display the stack 
-void	displayStack(struct Node* top) 
+void	display_stack(struct Node *top)
 {
-	if (isEmpty(top)) 
+	struct Node	*current;
+
+	current = top;
+	if (is_empty(top))
 	{
 		printf("Stack is empty!\n");
-		return;
-    } 
-    struct Node	*current = top;
-    printf("Stack: ");
-    while (current != NULL)
+		return ;
+	}
+	printf("Stack: ");
+	while (current != NULL)
 	{
 		printf("%d ", current->data);
 		current = current->next;
-    }
-	printf("\n"); 
+	}
+	printf("\n");
 }
 
-int main() 
-{ 
-    struct Node	*top = NULL;
-    push(&top, 10);
-    push(&top, 20);
-    push(&top, 30);
-    displayStack(top);
-    printf("%d popped from the stack.\n", pop(&top));
-    displayStack(top);
-    return (0);
+int	main(void)
+{
+	struct Node	*top;
+
+	top = NULL;
+	push(&top, 10);
+	push(&top, 20);
+	push(&top, 30);
+	displayStack(top);
+	printf("%d popped from the stack.\n", pop(&top));
+	display_stack(top);
+	return (0);
 }

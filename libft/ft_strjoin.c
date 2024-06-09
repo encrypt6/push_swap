8bf8e72692a21d61973aa3b186de358b12050cc4
Eliza Sikira <sikiraeliza2@gmail.com>
2024-06-09 12:01:32 +0100

    ft_strcat in ft_strjoin fixed

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,32 +6,11 @@
 /*   By: elsikira <elsikira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:54:16 by elsikira          #+#    #+#             */
-/*   Updated: 2024/04/16 17:04:09 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/06/09 12:01:08 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_strcat(char *dest, char *src)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {

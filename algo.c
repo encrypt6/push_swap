43e6bcd13256aae8580d1e226e1c3edbef691c69
Eliza Sikira <elsikira@paul-f4Br5s1.clusters.42paris.fr>
2024-06-26 17:33:05 +0200

    stack has its index, now sort index with radix

--- algo.c
+++ algo.c
@@ -6,13 +6,13 @@
 /*   By: elsikira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 14:52:54 by elsikira          #+#    #+#             */
-/*   Updated: 2024/06/26 15:33:35 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/06/26 17:32:11 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_sort_int_tab(int *tab, unsigned int size)
+int	*ft_sort_int_tab(int *tab, unsigned int size)
 {
 	unsigned int	i;
 	int				temp;
@@ -30,11 +30,14 @@ void	ft_sort_int_tab(int *tab, unsigned int size)
 		else
 			i++;
 	}
+	return (tab);
 }
 
-void	print_tab(int *tab, int	size)
+void	print_tab(int *tab, int size)
 {
-	int	i = 0;
+	int	i;
+
+	i = 0;
 	while (i < size)
 	{
 		printf("%d\n", tab[i]);
@@ -43,16 +46,15 @@ void	print_tab(int *tab, int	size)
 	printf("\n");
 }
 
-int	*ft_cpy_stack_to_tab(t_stack **a, int size) 
+int	*ft_cpy_stack_to_tab(t_stack **a, int size)
 {
-	int	i;
-	int	*tab;
+	int		i;
+	int		*tab;
 	t_stack	*temp;
-	
+
 	i = 0;
 	tab = malloc(sizeof(int) * size);
 	temp = *a;
-
 	if (!tab)
 		return (NULL);
 	while (temp != NULL)
@@ -61,37 +63,36 @@ int	*ft_cpy_stack_to_tab(t_stack **a, int size)
 		temp = temp->next;
 		i++;
 	}
-	//ft_sort_int_tab(tab, size); 
 	return (tab);
 }
 
-void	radix_sort(t_stack **a, t_stack **b)
+void	ft_indexation(t_stack **a)
 {
 	int	*tab;
 	int	size;
+	int	i;
 
-	*b = NULL;
 	size = ft_stack_size(*a);
-	tab = ft_cpy_stack_to_tab(a, size);
+	i = 0;
+	tab = ft_sort_int_tab(ft_cpy_stack_to_tab(a, size), size);
 	print_tab(tab, size);
-	ft_sort_int_tab(tab, size);
-	print_tab(tab, size);
-	/*while ((stack_size - 1) >> bit != 0)
-		bit++;
-	while (i < bit)
+	while ((*a))
 	{
-		j = 0;
-		while (j < stack_size)
+		while (tab[i])
 		{
-			if ((((*a)->index >> i) & 1) == 1)
-				ra(a, PRINT);
+			if (tab[i] == (*a)->value)
+			{
+				(*a)->index = i;
+				printf("%d\n", (*a)->index);
+				break ;
+			}
 			else
-				pb(b, a, PRINT);
+				i++;
 		}
-		while (b != NULL)
-			pa(a, b, PRINT);
-		i++;
-	}*/
+		i = 0;
+		(*a) = (*a)->next;
+	}
+	free(tab);
 }
 
 void	sort_two_three(t_stack **a)
@@ -99,7 +100,6 @@ void	sort_two_three(t_stack **a)
 	int	max_val;
 
 	max_val = ft_max_val(*a);
-
 	if ((*a)->value == max_val)
 		ra(a, PRINT);
 	else if ((*a)->next->value == max_val)
@@ -108,9 +108,14 @@ void	sort_two_three(t_stack **a)
 		sa(a, PRINT);
 }
 
-void	sort_algo(t_stack **a, t_stack **b)
+void	radix_sort(t_stack **a, t_stack **b)
 {
 	*b = NULL;
+	ft_indexation(a);
+}
+
+void	sort_algo(t_stack **a, t_stack **b)
+{
 	if ((*a) == NULL || (*a)->next == NULL)
 		exit(1);
 	else
@@ -118,8 +123,8 @@ void	sort_algo(t_stack **a, t_stack **b)
 		if (ft_stack_size(*a) <= 3)
 			sort_two_three(a);
 		else
-		{
 			radix_sort(a, b);
-		}
 	}
 }
+
+//8" "78" "2" "5" "6" "9" "7"
